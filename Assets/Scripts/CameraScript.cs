7964fed0ca8fbf5f43c554c537e2ff05cc2dc7ed
Gerda Fedotova <gerda.fedotova.sk@lvt.lv>
2025-10-20 18:57:36 +0300

    Camera zoom fix

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,18 +2,26 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public float maxZoom = 300f,
-        minZoom = 150f,
-        panSpeed = 6f;
-    Vector3 bottomLeft, topRight;
-    float cameraMaxX, cameraMinX, cameraMaxY, cameraMinY, x, y;
-    public Camera cam;
+    public float maxZoom = 300f;
+    public float minZoom = 150f;
+    public float panSpeed = 6f;
+    public float zoomStep = 50f;
+    public float zoomSmooth = 5f;
+    private Camera cam;
+    private float startZoom;
+    private float targetZoom;
+
+    private float x, y;
+    private Vector3 bottomLeft, topRight;
+    private float cameraMaxX, cameraMinX, cameraMaxY, cameraMinY;
 
     void Start()
     {
         cam = GetComponent<Camera>();
-        topRight = cam.ScreenToWorldPoint(
-            new Vector3(cam.pixelWidth, cam.pixelHeight, -transform.position.z));
+        startZoom = cam.orthographicSize;
+        targetZoom = startZoom;
+
+        topRight = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, -transform.position.z));
         bottomLeft = cam.ScreenToWorldPoint(new Vector3(0, 0, -transform.position.z));
         cameraMaxX = topRight.x;
         cameraMinX = bottomLeft.x;
@@ -21,49 +29,47 @@ public class CameraScript : MonoBehaviour
         cameraMinY = bottomLeft.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
         x = Input.GetAxis("Mouse X") * panSpeed;
         y = Input.GetAxis("Mouse Y") * panSpeed;
         transform.Translate(x, y, 0);
 
-        if((Input.GetAxis("Mouse ScrollWheel") > 0) && cam.orthographicSize > minZoom)
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (Mathf.Abs(scroll) > 0.001f)
         {
-            cam.orthographicSize = cam.orthographicSize - 50f;
+            Vector3 mouseWorldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            float oldSize = cam.orthographicSize;
+
+            if (scroll > 0 && targetZoom > minZoom)
+                targetZoom -= zoomStep;
+            else if (scroll < 0)
+            {
+                if (targetZoom < startZoom)
+                    targetZoom = Mathf.Min(targetZoom + zoomStep, startZoom);
+                else if (targetZoom < maxZoom)
+                    targetZoom += zoomStep;
+            }
+
+            cam.orthographicSize = targetZoom;
+
+            Vector3 newMouseWorldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 offset = mouseWorldPos - newMouseWorldPos;
+            transform.position += new Vector3(offset.x, offset.y, 0);
         }
 
-        if ((Input.GetAxis("Mouse ScrollWheel") < 0) && cam.orthographicSize < maxZoom)
-        {
-            cam.orthographicSize = cam.orthographicSize + 50f;
-        }
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomSmooth);
 
-        topRight = cam.ScreenToWorldPoint(
-            new Vector3(cam.pixelWidth, cam.pixelHeight, -transform.position.z));
+        topRight = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, -transform.position.z));
         bottomLeft = cam.ScreenToWorldPoint(new Vector3(0, 0, -transform.position.z));
 
         if (topRight.x > cameraMaxX)
-        {
-            transform.position = new Vector3(
-             transform.position.x - (topRight.x - cameraMaxX), transform.position.y, transform.position.z);
-        }
-
-        if(topRight.y > cameraMaxY)
-        {
-            transform.position = new Vector3(
-             transform.position.x, transform.position.y - (topRight.y - cameraMaxY), transform.position.z);
-        }
-
+            transform.position = new Vector3(transform.position.x - (topRight.x - cameraMaxX), transform.position.y, transform.position.z);
+        if (topRight.y > cameraMaxY)
+            transform.position = new Vector3(transform.position.x, transform.position.y - (topRight.y - cameraMaxY), transform.position.z);
         if (bottomLeft.x < cameraMinX)
-        {
-            transform.position = new Vector3(
-             transform.position.x + (cameraMinX - bottomLeft.x), transform.position.y, transform.position.z);
-        }
-
+            transform.position = new Vector3(transform.position.x + (cameraMinX - bottomLeft.x), transform.position.y, transform.position.z);
         if (bottomLeft.y < cameraMinY)
-        {
-            transform.position = new Vector3(
-             transform.position.x, transform.position.y + (cameraMinY - bottomLeft.y), transform.position.z);
-        }
+            transform.position = new Vector3(transform.position.x, transform.position.y + (cameraMinY - bottomLeft.y), transform.position.z);
     }
 }
