9545b3ab7c9e5a7fa2e61056a1e6b106df68def4
Gerda <gerda@gmail.com>
2025-10-27 14:39:48 +0200

    camera android

--- Assets/Scripts/DragAndDropScript.cs
+++ Assets/Scripts/DragAndDropScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragAndDropScript : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
+public class DragAndDropScript : MonoBehaviour, IBeginDragHandler, 
     IDragHandler, IEndDragHandler
 {
     private CanvasGroup canvasGro;
@@ -12,77 +12,107 @@ public class DragAndDropScript : MonoBehaviour, IPointerDownHandler, IBeginDragH
     public ScreenBoundriesScript screenBou;
     private int originalIndex;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 dragOffsetWorld;
+    private Camera uiCamera;
+    private Canvas canvas;
+
+    void Awake()
     {
         canvasGro = GetComponent<CanvasGroup>();
         rectTra = GetComponent<RectTransform>();
         canvasGro = GetComponent<CanvasGroup>();
         rectTra = GetComponent<RectTransform>();
         originalIndex = rectTra.GetSiblingIndex();
-    }
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        if (Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+        if(objectScr == null)
         {
-            Debug.Log("OnPointerDown");
-            objectScr.effects.PlayOneShot(objectScr.audioCli[0]);
-        } 
+            objectScr = Object.FindFirstObjectByType<ObjectScript>();
+        }
+
+        if (screenBou == null)
+        {
+            screenBou = Object.FindFirstObjectByType<ScreenBoundriesScript>();
+        }
+
+        canvas = GetComponentInParent<Canvas>();
+
+        if(canvas != null)
+        {
+            uiCamera = canvas.worldCamera;
+        }
+        else
+        {
+            Debug.LogError("Canvas not found fot DragAndDropScript >:(");
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-       if(Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+        ObjectScript.drag = true;
+        ObjectScript.lastDragged = eventData.pointerDrag;
+        canvasGro.blocksRaycasts = false;
+        canvasGro.alpha = 0.6f;
+        // rectTra.SetAsLastSibling();
+        int lastIndex = transform.parent.childCount - 1;
+        int position = Mathf.Max(0, lastIndex - 1);
+        transform.SetSiblingIndex(position);
+
+        Vector3 pointerWorld;
+        if(ScreenPointToWorld(eventData.position, out pointerWorld))
         {
-            ObjectScript.drag = true;
-            ObjectScript.lastDragged = eventData.pointerDrag;
-            canvasGro.blocksRaycasts = false;
-            canvasGro.alpha = 0.6f;
-            // rectTra.SetAsLastSibling();
-            int lastIndex = transform.parent.childCount - 1;
-            int position = Mathf.Max(0, lastIndex - 1);
-            transform.SetSiblingIndex(position);
-           Vector3 cursorWorldPos = Camera.main.ScreenToWorldPoint(
-               new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenBou.screenPoint.z));
-            rectTra.position = cursorWorldPos;
-
-            screenBou.screenPoint = Camera.main.WorldToScreenPoint(rectTra.localPosition);
-
-            screenBou.offset = rectTra.localPosition - 
-                Camera.main.ScreenToWorldPoint(
-                    new Vector3(Input.mousePosition.x, Input.mousePosition.y, 
-                screenBou.screenPoint.z));
+            dragOffsetWorld = transform.position - pointerWorld;
         }
+        else
+        {
+            dragOffsetWorld = Vector3.zero;
+        }
+
+        ObjectScript.lastDragged = eventData.pointerDrag;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
-        {
-            Vector3 curSreenPoint = 
-                new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenBou.screenPoint.z);
-            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curSreenPoint) + screenBou.offset;
-            rectTra.position = screenBou.GetClampedPosition(curPosition);
-        }
+        Vector3 pointerWorld;
+        if (!ScreenPointToWorld(eventData.position, out pointerWorld))
+            return;
+
+        Vector3 desiredPosition = pointerWorld + dragOffsetWorld;
+        desiredPosition.z = transform.position.z;
+
+        screenBou.RecalculateBounds();
+
+        Vector2 clamped = screenBou.GetClampedPosition(desiredPosition);
+        transform.position = new Vector3(clamped.x, clamped.y, desiredPosition.z);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (Input.GetMouseButtonUp(0))
-        {
-            ObjectScript.drag = false;
-            canvasGro.blocksRaycasts = true;
-            canvasGro.alpha = 1.0f;
+        ObjectScript.drag = false;
+        canvasGro.blocksRaycasts = true;
+        canvasGro.alpha = 1.0f;
 
-            if(objectScr.rightPlace)
-            {
-               canvasGro.blocksRaycasts = false;
-                ObjectScript.lastDragged = null;
-            }
-            
-            objectScr.rightPlace = false;
-            rectTra.SetSiblingIndex(originalIndex);
+        if (objectScr.rightPlace)
+        {
+            canvasGro.blocksRaycasts = false;
+            ObjectScript.lastDragged = null;
         }
+
+        objectScr.rightPlace = false;
+        rectTra.SetSiblingIndex(originalIndex);
+    }
+
+    private bool ScreenPointToWorld(Vector2 screenPoint, out Vector3 worldPoint)
+    {
+        worldPoint = Vector3.zero;
+        if (uiCamera == null)
+        {
+            return false;
+        }
+
+        float z = Mathf.Abs(uiCamera.transform.position.z - transform.position.z);
+        Vector3 sp = new Vector3(screenPoint.x, screenPoint.y, z);
+        worldPoint = uiCamera.ScreenToWorldPoint(sp);
+
+        return true;
     }
 }
--- Assets/Scripts/ScreenBoundriesScript.cs
+++ Assets/Scripts/ScreenBoundriesScript.cs
@@ -1,34 +1,129 @@
+using System;
 using UnityEngine;
 
 public class ScreenBoundriesScript : MonoBehaviour
 {
-    [HideInInspector]
-    public Vector3 screenPoint, offset;
-    [HideInInspector]
-    public float minX, maxX, minY, maxY;
+    [HideInInspector] public Vector3 screenPoint, offset;
+    [HideInInspector] public float minX, maxX, minY, maxY;
+    
+    public Rect worldBounds = new Rect(-683, -384, 1366, 768);
+    [Range(0f, 0.5f)]
     public float padding = 0.02f;
 
-   
+    public Camera targetCamera;
+
+    public float minCamX { get; private set; }
+    public float maxCamX { get; private set; }
+    public float minCamY { get; private set; }
+    public float maxCamY { get; private set; }
+
+    float lastOrthoSize;
+    float lastAspect;
+    Vector3 lastCamPos;
+
     void Awake()
-    {
-        Vector3 lowerLeft = Camera.main.ScreenToWorldPoint(Vector3.zero);
-        Vector3 upperRight = 
-            Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+    { 
 
-        float widthReduction = (upperRight.x - lowerLeft.x) * padding;
-        float heightReduction = (upperRight.y - lowerLeft.y) * padding;
-
-        minX = lowerLeft.x + widthReduction;
-        maxX = upperRight.x - widthReduction;
-        minY = lowerLeft.y + heightReduction;
-        maxY = upperRight.y - heightReduction;
+        if(targetCamera == null)
+        {
+            targetCamera = Camera.main;
+        }
     }
 
-    public Vector2 GetClampedPosition(Vector3 position)
+    void Update()
     {
-        return new Vector2(
-            Mathf.Clamp(position.x, minX, maxX),
-            Mathf.Clamp(position.y, minY, maxY)
-        );
+        if(targetCamera == null)
+        {
+            return;
+        }
+
+        bool changed = false;
+
+        if(targetCamera.orthographic)
+        {
+            if (!Mathf.Approximately(targetCamera.orthographicSize, lastOrthoSize))
+            {
+                changed = true;
+            }
+        }
+
+        if (!Mathf.Approximately(targetCamera.aspect, lastAspect))
+        {
+            changed = true;
+        }
+
+        if (targetCamera.transform.position !=lastCamPos)
+        {
+            changed = true;
+        }
+
+        if (changed)
+        {
+            RecalculateBounds();
+        }
+    }
+
+    public void RecalculateBounds()
+    {
+        if(targetCamera == null)
+        {
+            return;
+        }
+
+        float wbMinX = worldBounds.xMin;
+        float wbMaxX = worldBounds.xMax;
+        float wbMinY = worldBounds.yMin;
+        float wbMaxY = worldBounds.yMax;
+
+        if (targetCamera.orthographic)
+        {
+            float halfH = targetCamera.orthographicSize;
+            float halfW = halfH * targetCamera.aspect;
+            
+            if(halfW * 2f >= (wbMaxX - wbMinX))
+            {
+                minCamX = maxCamX = (wbMinX + wbMaxX);
+            }
+            else
+            {
+                minCamX = wbMinX + halfW;
+                maxCamX = wbMaxX - halfW;
+            }
+
+            if (halfH * 2f >= (wbMaxY - wbMinY))
+            {
+                minCamY = maxCamY = (wbMinY + wbMaxY);
+            }
+            else
+            {
+                minCamY = wbMinY + halfH;
+                maxCamY = wbMaxY - halfH;
+            }
+        }
+
+        lastOrthoSize = targetCamera.orthographicSize;
+        lastAspect = targetCamera.aspect;
+        lastCamPos = targetCamera.transform.position;
+    }
+
+    public Vector2 GetClampedPosition(Vector3 curPosition)
+    {
+        float shrinkW = worldBounds.width * padding;
+        float shrinkH = worldBounds.height * padding;
+        float wbMinX = worldBounds.xMin + shrinkW;
+        float wbMaxX = worldBounds.xMax - shrinkW;
+        float wbMinY = worldBounds.yMin + shrinkH;
+        float wbMaxY = worldBounds.yMax - shrinkH;
+        float cx = Mathf.Clamp(curPosition.x, wbMinX, wbMaxX);
+        float cy = Mathf.Clamp(curPosition.y, wbMinY, wbMaxY);
+
+        return new Vector2(cx, cy);
+    }
+
+    public Vector3 GetClampedCameraPosition(Vector3 desiredCamCenter)
+    {
+        float cx = Mathf.Clamp(desiredCamCenter.x, minCamX, maxCamX);
+        float cy = Mathf.Clamp(desiredCamCenter.y, minCamX, maxCamX);
+        return new Vector3(cx, cy, desiredCamCenter.z);
     }
 }
